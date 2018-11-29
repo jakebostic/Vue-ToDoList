2c71282e08f7e0ca165f3d662df8a8e8d59ba73b
jakebostic <jake_bostic@yahoo.com>
2018-11-29 16:32:08 -0500

    fixed completion functionality

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -36,7 +36,7 @@
     <div class='ui bottom attached green basic button' v-show="!isEditing && todo.done" disabled>
         Completed
     </div>
-    <div class='ui bottom attached red basic button' v-show="!isEditing && !todo.done">
+    <div class='ui bottom attached red basic button' v-show="!isEditing && !todo.done" v-on:click="completeTodo(todo)">
         Pending
     </div>
 </div>
